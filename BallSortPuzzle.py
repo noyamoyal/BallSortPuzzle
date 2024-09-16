6c1bd97c1ba74754a7a23a44d9226d6f917030d6
ido_eyali <ido.eyali@mail.huji.ac.il>
2024-09-16 13:37:59 +0300

    adding graphplan

--- BallSortPuzzle.py
+++ BallSortPuzzle.py
@@ -1,6 +1,8 @@
 import sys
 
 from BallSortGame import *
+# from GraphPlanning.domain_and_problem_factory import *
+# from GraphPlanning.planning_problem import *
 
 HUMAN = "human"
 AI = "AI"
@@ -16,32 +18,49 @@ def play_a_star_search(game, problem):
     return problem.expanded, game.get_steps()
 
 
+def play_graph_planning(game):
+    create_problem_file(game.get_tubes())
+    create_domain_file(game.get_tubes())
+    domain = 'GraphPlanning/domain.txt'
+    problem = 'GraphPlanning/problem.txt'
+    prob = PlanningProblem(domain, problem)
+    start = time.perf_counter()
+
+    plan = a_star_search(prob, heuristic)
+    elapsed = time.perf_counter() - start
+    if plan is not None:
+        print("Plan found with %d actions in %.2f seconds" % (len(plan), elapsed))
+        print([plane_.name for plane_ in plan])
+    else:
+        print("Could not find a plan in %.2f seconds" % elapsed)
+    print("Search nodes expanded: %d" % prob.expanded)
+    return prob.expanded, game.get_steps()
+
+
 def main():
     n_colors, agent, solve_plan = check_input_validity()
-    n_games = 2
+    n_games = 5
     expands, steps = 0, 0
 
     for i in range(n_games):
-
         game = BallSortGame(agent=agent, n_colors=n_colors)
         game_tube = game.get_tubes()
         if solve_plan:
             if solve_plan == SEARCH:
                 cur_expand, cur_steps = play_a_star_search(game, BallSortProblem(game_tube))
             else:
-                pass
+                cur_expand, cur_steps = play_graph_planning(game)
             expands += cur_expand
             steps += cur_steps
+        else:
+            game.game_runner()
+        if solve_plan:
             with open("results/summary.txt", 'a') as file:
                 file.write(f"\nfor agent :AI\n"
                            f"with n_colors:{n_colors}\n"
                            f"num of games : {n_games}\n"
                            f"average Expanded nodes: %d, average steps: %d\nusing:\n"
                            f"######\n" % (expands / n_games, steps / n_games))
-
-        else:
-            game.game_runner()
-        # cur_expand, cur_steps = game.game_runner()
 
 
 def check_input_validity():
@@ -72,5 +91,3 @@ def check_input_validity():
 
 if __name__ == '__main__':
     main()
-
-
